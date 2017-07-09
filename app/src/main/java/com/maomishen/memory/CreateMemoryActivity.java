f6b20d4769b6ffb225f393338a15f9b6680f21ea
LunaGao <gao.xue.qi@qq.com>
2017-07-09 15:00:31 +0800

    fix CI and set create View title and back button

--- app/src/main/java/com/maomishen/memory/CreateMemoryActivity.java
+++ app/src/main/java/com/maomishen/memory/CreateMemoryActivity.java
@@ -3,6 +3,8 @@ package com.maomishen.memory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
 
 public class CreateMemoryActivity extends AppCompatActivity {
 
@@ -13,5 +15,19 @@ public class CreateMemoryActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_create_memory_activity);
         setSupportActionBar(toolbar);
+        try {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        } catch (Exception ex) {
+            Log.e("error", ex.getMessage());
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
