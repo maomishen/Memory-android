bec35259165462d288d6883ffc7d15293b81ab02
LunaGao <gao.xue.qi@qq.com>
2017-07-09 14:51:10 +0800

    fix CI

--- app/src/main/java/com/maomishen/memory/CreateMemoryActivity.java
+++ app/src/main/java/com/maomishen/memory/CreateMemoryActivity.java
@@ -1,7 +1,8 @@
 package com.maomishen.memory;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class CreateMemoryActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class CreateMemoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_memory);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_create_memory_activity);
+        setSupportActionBar(toolbar);
     }
 }
